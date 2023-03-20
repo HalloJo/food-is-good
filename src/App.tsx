3632518f2b066e751c464192fe49ceaee90c90ea
HalloJo <jorikvanruiswijk@gmail.com>
2023-03-20 11:03:51 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Food is good.</h1>
+    </div>
+  );
+}
+
+export default App;
